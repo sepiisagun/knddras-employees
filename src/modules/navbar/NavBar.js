b46442500b225feb38a68b09efa1d2bc5d5d1d84
Rick Jone <villanuevarickjone0115@gmail.com>
2023-10-04 01:35:53 +0800

    Fixed Navigation padding to fil the design and added Navigation Icon

--- src/modules/navbar/NavBar.js
+++ src/modules/navbar/NavBar.js
@@ -1,24 +1,31 @@
-import { Box, Flex, HStack, Spacer } from "@chakra-ui/react";
+import { Box, Flex, HStack, Icon, Spacer } from "@chakra-ui/react";
 import React from "react";
+import { RiFlashlightFill } from "react-icons/ri";
 
 const NavBar = () => {
 	const iconsize = "40px";
+	const px = "10px";
+	const py = "15px";
 	return (
 		<Flex
 			alignItems="center"
 			borderBottom="1px"
 			borderBottomColor="gray.300"
-			px="30px"
-			py="20px"
+			px={px}
+			py={py}
 		>
 			<HStack>
-				<Box
+				<Flex
+					alignItems="center"
 					bg="teal.300"
 					borderRadius="50px"
 					h={iconsize}
+					justifyContent="center"
 					mr="10px"
 					w={iconsize}
-				/>
+				>
+					<Icon as={RiFlashlightFill} color="white" h={8} w={8} />
+				</Flex>
 				<Box fontWeight="medium">Klinika ng Dentista</Box>
 			</HStack>
 			<Spacer />
