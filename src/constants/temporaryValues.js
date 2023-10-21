c262ca3901ed56f37f023f37ca936b74ba88ab0e
SheddC <20197474@s.ubaguio.edu>
2023-10-21 19:46:15 +0800

    feat: created Dashboard Appointments table

---
+++ src/constants/temporaryValues.js
@@ -0,0 +1,36 @@
+const APPOINTMENT_USERS = [
+	{
+		date: "April 01, 2023",
+		doctor: "Dr. John Doe",
+		name: "Juan Dela Cruz",
+		procedure: "Root Canal Theraphy",
+		status: "Scheduled",
+		time: "10:00 AM - 11:00 AM",
+	},
+	{
+		date: "April 01, 2023",
+		doctor: "Dr. John Doe",
+		name: "Juan Dela Cruz",
+		procedure: "Root Canal Theraphy",
+		status: "Scheduled",
+		time: "10:00 AM - 11:00 AM",
+	},
+	{
+		date: "April 01, 2023",
+		doctor: "Dr. John Doe",
+		name: "Juan Dela Cruz",
+		procedure: "Root Canal Theraphy",
+		status: "Ended",
+		time: "10:00 AM - 11:00 AM",
+	},
+	{
+		date: "April 01, 2023",
+		doctor: "Dr. John Doe",
+		name: "Juan Dela Cruz",
+		procedure: "Root Canal Theraphy",
+		status: "Ended",
+		time: "10:00 AM - 11:00 AM",
+	},
+];
+
+export default APPOINTMENT_USERS;
