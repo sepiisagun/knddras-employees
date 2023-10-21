10ebceb31e01b405a27a2f17d2b0dabb445af9c2
SheddC <20197474@s.ubaguio.edu>
2023-10-21 20:47:29 +0800

    feat: created Dashboard Redirect Banner component

---
+++ src/modules/dashboard/components/RedirectBanner.js
@@ -0,0 +1,53 @@
+import {
+	Button,
+	Card,
+	CardBody,
+	CardFooter,
+	Heading,
+	Image,
+	Stack,
+	Text,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import spiels from "../../../constants/spiels";
+
+const RedirectBanner = () => {
+	return (
+		<Card
+			direction={{ base: "column", sm: "row" }}
+			overflow="hidden"
+			variant="outline"
+		>
+			<Stack>
+				<CardBody>
+					<Text color="#A0AEC0" py="2">
+						{spiels.REDIRECT_SUBHEADER}
+					</Text>
+					<Heading size="md">{spiels.REDIRECT_BANNER}</Heading>
+					<Text color="#A0AEC0" py="2">
+						{spiels.REDIRECT_BANNER_DESC}
+					</Text>
+				</CardBody>
+
+				<CardFooter>
+					<Button
+						colorScheme="blue"
+						rightIcon={<ArrowForwardIcon mt={1} />}
+						variant="ghost"
+					>
+						Open
+					</Button>
+				</CardFooter>
+			</Stack>
+			<Image
+				alt="Redirect Banner"
+				maxW={{ base: "100%", sm: "600px" }}
+				objectFit="cover"
+				p={4}
+				src="RedirectBanner.png"
+			/>
+		</Card>
+	);
+};
+
+export default RedirectBanner;
