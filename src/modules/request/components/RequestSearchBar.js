30a697d9e6b7b71a488dbbd5376adcd4ac6ef882
SheddC <20197474@s.ubaguio.edu>
2023-12-04 16:02:31 +0800

    feat: created request screen initial layout

---
+++ src/modules/request/components/RequestSearchBar.js
@@ -0,0 +1,45 @@
+import {
+	Box,
+	Button,
+	Flex,
+	HStack,
+	Icon,
+	Input,
+	InputGroup,
+	InputLeftElement,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { MdFilterList } from "react-icons/md";
+import spiels from "../../../constants/spiels";
+
+const RequestSearchBar = () => {
+	const [showFilter, setShowFilter] = useState(false);
+	return (
+		<HStack py={3}>
+			<InputGroup>
+				<InputLeftElement pointerEvents="none">
+					<SearchIcon boxSize={4} />
+				</InputLeftElement>
+				<Input
+					borderRadius="xl"
+					placeholder={spiels.PLACEHOLDER_SEARCH}
+					type="text"
+				/>
+			</InputGroup>
+
+			<Button
+				onClick={() => {
+					setShowFilter(!showFilter);
+				}}
+			>
+				<Flex justifyContent="center" mr={2}>
+					<Icon as={MdFilterList} h={5} w={5} />
+				</Flex>
+				<Box justifyContent="center">{spiels.TEXT_FILTER}</Box>
+			</Button>
+		</HStack>
+	);
+};
+
+export default RequestSearchBar;
--- src/modules/request/screens/RequestScreen.js
+++ src/modules/request/screens/RequestScreen.js
@@ -1,10 +1,26 @@
-import SearchBar from "../../../components/Search/SearchBar";
+import { Box } from "@chakra-ui/react";
 import ProfileLayout from "../../../layout/ProfileLayout";
+import RequestSearchBar from "../components/RequestSearchBar";
+import StrapiTable from "../../../components/Table";
 
 const RequestScreen = () => {
 	return (
 		<ProfileLayout>
-			<SearchBar />
+			<Box maxW="auto" p={{ base: 4, md: 5 }}>
+				<RequestSearchBar />
+				<StrapiTable
+					action={["View"]}
+					headerTitles={[
+						"Name",
+						"Procedure",
+						"Contact Number",
+						"Prospect Schedule",
+						"Status",
+						"Action",
+					]}
+					title="CTTable"
+				/>
+			</Box>
 		</ProfileLayout>
 	);
 };
