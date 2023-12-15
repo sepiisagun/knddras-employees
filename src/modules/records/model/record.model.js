e28849c29fdf97a7809b726f423eedfb24a589b2
sepiisagun <sherman.sagun@gmail.com>
2023-12-15 10:19:44 +0800

    fix: patient info validation

--- src/modules/records/model/record.model.js
+++ src/modules/records/model/record.model.js
@@ -1,4 +1,4 @@
-import { date, object, string } from "yup";
+import { boolean, date, object, string } from "yup";
 
 export const phoneLength = /^[-]*(?:.[-]*){7,12}$/;
 
@@ -6,17 +6,38 @@ export const phoneRegExp =
 	/^((\\+[1-9]{1,4}[ \\-]?)|(\\([0-9]{2,3}\\)[ \\-]?)|([0-9]{2,4})[ \\-]?)*?[0-9]{3,4}?[ \\-]?[0-9]{3,4}?$/;
 
 const recordSchema = {
+	address: string().required("Please enter your address"),
+	birthdate: date().required("Please enter your birthdate"),
 	email: string()
 		.email("Please enter a valid email address")
 		.required("Please enter your email address"),
 	firstName: string().required("Please enter your first name"),
-
+	guardianName: string().when("minor", {
+		is: true,
+		then: () => string().required("Please enter your Guardian's full name"),
+	}),
 	lastName: string().required("Please enter your last name"),
-
+	middleInitial: string()
+		.required("Please enter your middle initial")
+		.max(1, "Maximum of one letter only"),
+	minor: boolean(),
 	mobileNumber: string()
 		.matches(phoneLength, "Contact Number must be 7 to 12 character length")
 		.matches(phoneRegExp, "Invalid Contact Number")
 		.required("Please enter your contact number"),
+	nationality: string().nullable(),
+	occupation: string().nullable(),
+	relation: string().when("minor", {
+		is: true,
+		then: () =>
+			string().required(
+				"Please enter your relationship with your Guardian.",
+			),
+	}),
+	religion: string().nullable(),
+	sex: string()
+		.oneOf(["MALE", "FEMALE", "OTHER"], "Invalid selection")
+		.required("Please select your sex"),
 };
 
 const medicalSchema = {
